d681ce86e936ebaddeaf6cb7c9cbbb40bb24eb0b
ClemSpy <spy_c@hotmail.fr>
2017-03-01 10:39:09 +0100

    Ajout de Optics et HC

--- Clustering/src/HierarchiqueAscendante.java
+++ Clustering/src/HierarchiqueAscendante.java
@@ -1,4 +1,12 @@
+import weka.clusterers.HierarchicalClusterer;
 
 public class HierarchiqueAscendante {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HierarchicalClusterer HC = new HierarchicalClusterer();
+		
+	}
+	
 }
