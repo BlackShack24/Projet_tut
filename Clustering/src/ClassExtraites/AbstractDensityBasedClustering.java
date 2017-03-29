5d74c5c19c5356780775bd3c0d4f1655d53a66a5
ClemSpy <spy_c@hotmail.fr>
2017-03-29 14:19:45 +0200

    Changement temps DBSCAN 8

--- Clustering/src/ClassExtraites/AbstractDensityBasedClustering.java
+++ Clustering/src/ClassExtraites/AbstractDensityBasedClustering.java
@@ -9,79 +9,77 @@ import net.sf.javaml.distance.DistanceMeasure;
 
 abstract class AbstractDensityBasedClustering
 {
-  DistanceMeasure dm;
-  
-  class DataObject
-  {
-    int clusterIndex = -1;
-    double c_dist;
-    double r_dist;
-    boolean processed = false;
-    static final int UNCLASSIFIED = -1;
-    static final int UNDEFINED = Integer.MAX_VALUE;
-    static final int NOISE = -2;
-    Instance instance;
-    
-    public DataObject(DensityBasedSpatialClustering densityBasedSpatialClustering, Instance inst)
-    {
-      this.instance = inst;
-    }
-    
-    public DataObject(Instance inst)
-    {
-      this.instance = inst;
-    }
-    
-    public boolean equals(Object obj)
-    {
-      DataObject tmp = (DataObject)obj;
-      return tmp.instance.equals(this.instance);
-    }
-    
-    public int hashCode()
-    {
-      return this.instance.hashCode();
-    }
-    
-    public String getKey()
-    {
-      return this.instance.toString();
-    }
-  }
-  
-  Vector<DataObject> dataset = null;
-  
-  long debut=0, debut1=0, debut2=0, debut3=0;
-  
-  List<DataObject> epsilonRangeQuery(double epsilon, DataObject inst)
-  {
-    ArrayList<DataObject> epsilonRange_List = new ArrayList();
-	
-    for (int i = 0; i < this.dataset.size(); i++)
-    {
-      debut = System.currentTimeMillis(); // time
-      DataObject tmp = (DataObject)this.dataset.get(i);
-      debut1 += System.currentTimeMillis()-debut; // time
-      debut2 = System.currentTimeMillis(); // time
-      double distance = this.dm.measure(tmp.instance, inst.instance);
-      if (distance < epsilon) {
-        epsilonRange_List.add(tmp);
-      }
-      debut3 += System.currentTimeMillis()-debut2; // time
-    }
-    return epsilonRange_List;
-  }
+	DistanceMeasure dm;
 
-public long getDebut1() {
-	return debut1;
-}
+	class DataObject
+	{
+		int clusterIndex = -1;
+		double c_dist;
+		double r_dist;
+		boolean processed = false;
+		static final int UNCLASSIFIED = -1;
+		static final int UNDEFINED = Integer.MAX_VALUE;
+		static final int NOISE = -2;
+		Instance instance;
 
-public long getDebut3() {
-	return debut3;
-}
-  
-  
-  
-  
-  
+		public DataObject(DensityBasedSpatialClustering densityBasedSpatialClustering, Instance inst)
+		{
+			this.instance = inst;
+		}
+
+		public DataObject(Instance inst)
+		{
+			this.instance = inst;
+		}
+
+		public boolean equals(Object obj)
+		{
+			DataObject tmp = (DataObject)obj;
+			return tmp.instance.equals(this.instance);
+		}
+
+		public int hashCode()
+		{
+			return this.instance.hashCode();
+		}
+
+		public String getKey()
+		{
+			return this.instance.toString();
+		}
+	}
+
+	Vector<DataObject> dataset = null;
+
+	long debut=0, debut1=0, debut2=0, debut3=0;
+
+	List<DataObject> epsilonRangeQuery(double epsilon, DataObject inst)
+	{
+		ArrayList<DataObject> epsilonRange_List = new ArrayList();
+
+		for (int i = 0; i < this.dataset.size(); i++)
+		{		
+			DataObject tmp = (DataObject)this.dataset.get(i);
+			debut = System.currentTimeMillis(); // time
+			double distance = this.dm.measure(tmp.instance, inst.instance);
+			debut1 += System.currentTimeMillis()-debut; // time
+			debut2 = System.currentTimeMillis(); // time
+			if (distance < epsilon) epsilonRange_List.add(tmp);
+			debut3 += System.currentTimeMillis()-debut2; // time
+		}
+		return epsilonRange_List;
+	}
+
+	public long getDebut1() {
+		return debut1;
+	}
+
+	public long getDebut3() {
+		return debut3;
+	}
+
+
+
+
+
 }
